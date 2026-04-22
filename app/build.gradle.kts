f68ad7441c6d4be30d92106cd21701e006885483
Juan Diego Velasco <velasco.juan.7583@eam.edu.co>
2026-04-21 22:40:53 -0500

    Dependencias: navigation-compose y coil agregadas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
 }
